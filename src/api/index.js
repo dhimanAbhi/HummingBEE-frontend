1b6c71ea94e146593f40ebc860ded960b7103117
abhi2002dhi <abhi2002dhiman@gmail.com>
2024-08-01 21:39:08 +0530

    frontend

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,2 @@
-export const apiUrl = 'http://localhost:4000';
+// export const apiUrl = 'http://localhost:4000';
+export const apiUrl = "https://humming-bee-server-ra5aorh8w-abhishek-dhimans-projects-f1fd5dda.vercel.app/"
