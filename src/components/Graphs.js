b7133fadfba038fe0579633f5f71fd25ec1bf3b8
abhi2002dhi <abhi2002dhiman@gmail.com>
2024-09-16 20:44:12 +0530

    frontend

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -308,10 +308,10 @@ const setCheckedInOut = async (newCheckStatus) => {
                   Lorem ipsum odor amet, consectetuer adipiscing elit. Vivamus hac feugiat eros ridiculus at sagittis. Erat justo dapibus nullam senectus aptent vehicula vehicula. Dis inceptos rutrum lacinia dui scelerisque sapien ultrices volutpat
                   </div>
               </div>
-              {/* <div className='google-slack'>
+              <div className='google-slack'>
                 <div style={{borderRight:"1px solid white"}}>Google Calendar <a href='https://calendar.google.com/'><img src="../images/google-calendar-new.svg" className='google-slack-img'/></a></div>
                 <div>Slack <a href="https://slack.com/"><img src="../images/slack-new.svg" className='google-slack-img'/></a></div>
-              </div> */}
+              </div>
           </div>
           <div className='todo-box'>
             <Todo />
